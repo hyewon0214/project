5c48ed9bf33737d43d50ff215f39d6ef9bc3ef18
hyewon0214 <83756966+hyewon0214@users.noreply.github.com>
2021-05-11 12:21:57 +0900

    Add files via upload

---
+++ d.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import urllib.request
+response=urllib.request.urlopen('https://sports.news.naver.com/kbaseball/record/index.nhn?category=kbo&year=2020')
+soup=BeautifulSoup(response,'html.parser')
+data=[]
+for tr_tag in soup.find(id='regularTeamRecordList_table').find_all('tr'):
+    th_tag=tr_tag.find('th')
+    strong_tag=th_tag.find('strong')
+    lank=strong_tag.get_text()
+
+    span_tag=tr_tag.findAll('span')
+    team=span_tag[0].get_text()
+    total=span_tag[1].get_text()
+    win=span_tag[2].get_text()
+    lose=span_tag[3].get_text()
+    draw=span_tag[4].get_text()
+    data.append([team,lank,total,win,lose,draw])
+print(data)
+
