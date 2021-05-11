0804841aa8ef90e1921f9e7da8a29cdb4fc068cd
hyewon0214 <83756966+hyewon0214@users.noreply.github.com>
2021-05-11 16:24:48 +0900

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('baseball.csv',index_col='team',encoding='euc-kr')
+#print(df)
+font_name=mpl.font_manager.FontProperties(fname='C:/Windows/Fonts/malgun.ttf').get_name()
+mpl.rc('font',family=font_name)
+ax=df.plot(kind='bar',figsize=(12,4),legend=True, fontsize=12)
+ax.set_xlabel('구단',fontsize=12)
+ax.set_ylabel('lank/total',fontsize=12)
+ax.legend(['lank','total'], fontsize=12)
+plt.show()
